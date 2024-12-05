3da6c0e2814c677711c2a6ca74582189dafb31d1
Rodrigo Tumani <149439452+RodTumani@users.noreply.github.com>
2024-12-05 17:36:31 -0600

    changed mobile responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function togglemenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+
+    if (menu.classList.contains("open")) {
+        menu.style.maxHeight = "0";
+    } else {
+        menu.style.maxHeight = `${menu.scrollHeight}px`;
+    }
+
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
